0b035e5b3914eff046233da73313ba61bbc14f5d
luoyusoft <luoyusoft@126.com>
2021-02-12 20:57:03 +0800

    first commit

---
+++ src/main/java/com/luoyu/java/JavaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.luoyu.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luoyu/java/JavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luoyu.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
