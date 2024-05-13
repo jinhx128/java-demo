e36ffca38c4c8d23ede287abf4e18dd90b1313ed
jinhaoxun <jinhaoxun@zhuanzhuan.com>
2024-05-13 11:17:54 +0800

    1. 新增RedisListener

---
+++ src/main/java/com/jinhx/java/other/redis/RedisListener.java
@@ -0,0 +1,31 @@
+package com.jinhx.java.other.redis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.DefaultMessage;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+/**
+ * RedisListener
+ *
+ * @author jinhx
+ * @since 2021-12-31
+ */
+@Slf4j
+public class RedisListener extends KeyExpirationEventMessageListener {
+
+    public RedisListener(RedisMessageListenerContainer listenerContainer) {
+        super(listenerContainer);
+    }
+
+    @Override
+    protected void doHandleMessage(Message message) {
+        Message defaultMessage = new DefaultMessage(message.getChannel(), message.getBody());
+        // 获取到key
+        String key = defaultMessage.toString();
+        log.info("act=RedisListener key={} body={} channel={}", key, defaultMessage.getBody(), defaultMessage.getChannel());
+        super.doHandleMessage(message);
+    }
+
+}
