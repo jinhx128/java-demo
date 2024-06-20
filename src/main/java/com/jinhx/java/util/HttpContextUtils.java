17a3d3fff3b9eda8500120c169acf4bfd6b77df0
jinhaoxun <jinhaoxun@zhuanzhuan.com>
2024-06-20 10:37:09 +0800

    1. 新增HttpContext工具

---
+++ src/main/java/com/jinhx/java/util/HttpContextUtils.java
@@ -0,0 +1,31 @@
+package com.jinhx.java.util;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * HttpContextUtils
+ *
+ * @author jinhx
+ * @since 2021-10-06
+ */
+public class HttpContextUtils {
+
+	public static HttpServletRequest getHttpServletRequest() {
+		return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+	}
+
+	public static String getDomain(){
+		HttpServletRequest request = getHttpServletRequest();
+		StringBuffer url = request.getRequestURL();
+		return url.delete(url.length() - request.getRequestURI().length(), url.length()).toString();
+	}
+
+	public static String getOrigin(){
+		HttpServletRequest request = getHttpServletRequest();
+		return request.getHeader("Origin");
+	}
+
+}
